736fb5b22ae763e821514ae43a1af71143f07a7e
Ayla Basha <aylabasha@gmail.com>
2022-10-09 23:15:32 -0700

    task list

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from "expo-status-bar";
+
 import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import Task from "./components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on yo!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}> 
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+          <Task text={'Task 1'} />
+          <Task text={'Task 2'} />
+          </View>
+      </View>
     </View>
   );
 }
@@ -13,6 +21,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#E8EAD",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
   },
 });
